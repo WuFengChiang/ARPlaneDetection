c0b58b4ee9a165d7abc9a7f26c2c5c5817006092
wuufone <wuufone@gmail.com>
2019-07-22 22:22:41 +0800

    Initial Commit

---
+++ ARPlaneDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARPlaneDetection
+//
+//  Created by wuufone on 2019/7/22.
+//  Copyright © 2019 江武峯. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
